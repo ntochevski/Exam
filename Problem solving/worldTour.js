c64488e4d0e0ea467887fac9b25449ed2074e8b0
ntochevski <tochevski.nikolay@gmail.com>
2021-11-24 16:00:05 +0200

    New exercise

---
+++ Problem solving/worldTour.js
@@ -0,0 +1,49 @@
+function solve(arr) {
+    let stopString = arr.shift();
+
+    let command = arr.shift().split(':');
+
+    while (command[0] !== "Travel") {
+        if (command[0] === 'Add Stop') {
+            if (command[1] >= 0 && command[1] < stopString.length) {
+                addStop(stopString, command);
+            }
+        } else if (command[0] === 'Remove Stop') {
+            if ((command[1] >= 0 && command[1] < stopString.length) && (command[2] >= 0 && command[2] < stopString.length)) {
+                removeStop(stopString, command);
+            }
+        } else {
+            while (stopString.includes(command[1])) {
+                stopString = stopString.replace(command[1], command[2])
+            }
+        }
+        console.log(stopString);
+        command = arr.shift().split(':');
+    }
+    console.log(`Ready for world tour! Planned stops: ${stopString}`);
+
+    function addStop(input, command) {
+        stopString = stopString.split('');
+        stopString.splice(Number(command[1]), 0, command[2]);
+        stopString = stopString.join('');
+        return stopString;
+
+    }
+    function removeStop(input, command) {
+        stopString = stopString.split('');
+        let startIndex = command[1];
+        let deletCount = Math.abs(command[1] - command[2]);
+        stopString.splice(startIndex, ++deletCount);
+        stopString = stopString.join('');
+        return stopString;
+    }
+}
+solve((["Hawai::Cyprys-Greece",
+
+    "Add Stop:7:Rome",
+
+    "Remove Stop:11:16",
+
+    "Switch:Hawai:Bulgaria",
+
+    "Travel"]))
